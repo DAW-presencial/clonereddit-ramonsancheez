f01f737118269c2d8260453a017c5e1b4542044c
ramonsancheez <rsanchezabadia@cifpfbmoll.eu>
2023-02-13 18:00:58 +0000

    primer commit con migraciones

---
+++ database/migrations/2023_02_13_175127_create_posts_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('posts', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->string('extract');
+            $table->string('content');
+            $table->boolean('expirable');
+            $table->boolean('caducable');
+            $table->boolean('comentable');
+            $table->string('access');
+            $table->foreignId('user_id') -> constrained();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('posts');
+    }
+};
